fce34b234876efa5064a9eb436949c7d27044b00
Chris <bahrango@hotmail.com>
2019-01-09 13:56:02 -0700

    initial commit

---
+++ src/FabulousRepro.Gtk/Main.fs
@@ -0,0 +1,19 @@
+﻿namespace FabulousRepro.Gtk
+    module Main =
+
+        open Xamarin.Forms
+        open Xamarin.Forms.Platform.GTK
+
+        [<EntryPoint>]
+        let Main(args) =
+            Gtk.Application.Init()
+            Forms.Init()
+
+            let app = new FabulousRepro.App()
+            let window = new FormsWindow()
+            window.LoadApplication(app)
+            window.SetApplicationTitle("Hello Fabulous Linux")
+            window.Show();
+
+            Gtk.Application.Run()
+            0
---
+++ src/FabulousRepro/App.fs
@@ -0,0 +1,25 @@
+﻿namespace FabulousRepro
+
+open Fabulous.Core
+open Fabulous.DynamicViews
+open Xamarin.Forms
+
+module App =
+    type Model = { Prop : string }
+    let initModel = { Prop = "" }
+    let init () = initModel, Cmd.none    
+    let update msg model = model, Cmd.none
+    let view (model: Model) dispatch = View.ContentPage(content = View.Label("can't"))
+
+type App () as app = 
+    inherit Application ()
+    
+    let runner =
+        Program.mkProgram App.init App.update App.view
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> Program.runWithDynamicView app
+
+
+
